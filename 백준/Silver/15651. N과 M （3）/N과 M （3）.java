9609138ccae453b9980f13e70732ead7bc5c635f
SunghoCha <61485641+SunghoCha@users.noreply.github.com>
2025-01-28 01:03:15 +0900

    [Silver III] Title: N과 M (3), Time: 412 ms, Memory: 68408 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,53 @@
+
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+    private static boolean[] visited;
+    private static int[] sequence;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+        sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        sequence = new int[M];
+        int depth = 0;
+        backTrack(depth);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void backTrack(int depth) {
+        if (depth == M) {
+            for (int value : sequence) {
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sequence[depth] = i;
+            backTrack(depth + 1);
+        }
+
+
+    }
+}
