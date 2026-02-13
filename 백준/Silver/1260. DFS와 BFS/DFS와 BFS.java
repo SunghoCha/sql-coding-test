efa8075721a2a32c99c082094173b124c001b151
SunghoCha <61485641+SunghoCha@users.noreply.github.com>
2026-02-14 01:22:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 188 ms, Memory: 20040 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,99 +1,120 @@
 
 
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
+
+    private static int N, M, S;
+    private static int count = 1;
     private static StringBuilder sb;
     private static boolean[] visited;
-    private static List<Integer> visitedOrder;
-    private static List<Integer>[] neighbors;
+    private static int[] visitedOrder;
+    private static Node[] nodes;
+    private static Deque<Node> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         sb = new StringBuilder();
-
+        ///////////////////////////////////////////////////////////////////////////
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int a = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-        // 초기화
-        visitedOrder = new ArrayList<>();
-        visited = new boolean[n + 1];
-        neighbors = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            neighbors[i] = new ArrayList<>();
+        nodes = new Node[N + 1];
+        // 노드 생성
+        for (int i = 1; i <= N; i++) {
+            nodes[i] = new Node(i);
         }
-
-        // 노드 연결
-        for (int i = 1; i <= m; i++) {
+        // 간선 연결
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int node = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            neighbors[node].add(value);
-            neighbors[value].add(node); // 양방향
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            nodes[a].addAdjNode(nodes[b]);
+            nodes[b].addAdjNode(nodes[a]);
         }
-
         // 오름차순 정렬
-        for (List<Integer> list : neighbors) {
-            list.sort((a1, b1) -> a1 - b1);
+        for (int i = 1; i <= N; i++) {
+            nodes[i].getAdjNodes().sort((a, b) -> a.getNumber() - b.getNumber());
         }
 
-        dfs(a);
-        for (Integer value : visitedOrder) {
-            sb.append(value).append(" ");
-        }
+        visited = new boolean[N + 1];
+        dfs(nodes[S]);
 
+        ///////
         sb.append("\n");
-        resetGraph();
+        count = 1;
+        visited = new boolean[N + 1];
+        bfs();
 
-        bfs(a);
-        for (Integer value : visitedOrder) {
-            sb.append(value).append(" ");
-        }
 
+        ///////////////////////////////////////////////////////////////////////////
         bw.write(sb.toString());
         bw.flush();
+        bw.close();
+
     }
 
-    private static void dfs(int start) {
-        dfsRecursive(start);
-    }
-
-    private static void dfsRecursive(int start) {
-        visited[start] = true;
-        visitedOrder.add(start);
-
-        for (Integer value : neighbors[start]) {
-            if (!visited[value]) {
-                dfsRecursive(value);
+    private static void dfs(Node node) {
+        int number = node.getNumber();
+        if (!visited[number]) {
+            visited[number] = true;
+            sb.append(number).append(" ");
+            List<Node> adjNodes = node.getAdjNodes();
+            for (Node adjNode : adjNodes) {
+                dfs(adjNode);
             }
         }
     }
 
-    private static void resetGraph() {
-        Arrays.fill(visited, false);
-        visitedOrder.clear();
-    }
+    private static void bfs() {
+        queue.addLast(nodes[S]);
+        visited[S] = true;
+        sb.append(nodes[S].getNumber()).append(" ");
 
-    private static void bfs(int start) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.offer(start);
-        visited[start] = true;
-        visitedOrder.add(start);
-
-        while (!deque.isEmpty()) {
-            Integer node = deque.poll();
-            for (Integer value : neighbors[node]) {
-                if (!visited[value]) {
-                    deque.offer(value);
-                    visited[value] = true;
-                    visitedOrder.add(value);
+        while (!queue.isEmpty()) {
+            Node node = queue.pollFirst();
+            List<Node> adjNodes = node.getAdjNodes();
+            for (Node adjNode : adjNodes) {
+                int number = adjNode.getNumber();
+                if (!visited[number]) {
+                    visited[number] = true;
+                    sb.append(number).append(" ");
+                    queue.addLast(adjNode);
                 }
             }
         }
     }
+
+
+    public static class Node {
+        int number;
+        List<Node> adjNodes = new ArrayList<>();
+
+        public Node(int number) {
+            this.number = number;
+        }
+
+        public void addAdjNode(Node node) {
+            adjNodes.add(node);
+        }
+
+        public List<Node> getAdjNodes() {
+            return adjNodes;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+    }
+
+
+
+
+
+
 }
