c88677c3acfcc348e3ea7a6bf135482f85f25430
SunghoCha <61485641+SunghoCha@users.noreply.github.com>
2026-02-22 11:41:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        int i = 0;
+        int j = 0;
+        for (String str : goal) {
+            if (i < cards1.length && cards1[i].equals(str)) {
+                i++;
+            } else if (j < cards2.length && cards2[j].equals(str)) {
+                j++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}
