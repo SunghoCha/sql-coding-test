3552056c3f8efcfe1f778da585916b75e0567be4
SunghoCha <61485641+SunghoCha@users.noreply.github.com>
2026-03-12 09:49:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 20.59 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,42 +2,43 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int n = s.length();
-        char[] doubles = new char[2 * n];
         char[] chars = s.toCharArray();
+        int n = chars.length;
+        char[] doubles = new char[2 * n];
+        
         for (int i = 0; i < 2 * n; i++) {
-            doubles[i] = chars[i % n]; 
+            doubles[i] = chars[i % n];
         }
-        /*
-            i: 0 ~ n - 1 까지 이동시키면서 사이즈 n짜리 윈도우에 대해서 스택으로 대칭 검증
-        */
-        for (int i = 0; i < n; i++) { // 시작 인덱스
-            boolean isOk = true;
+        
+        for (int i = 0; i < n; i++) { // 시작지점 루프
             Deque<Character> stack = new ArrayDeque<>();
+            boolean isValid = true;
             for (int j = i; j < i + n; j++) {
-                
                 char ch = doubles[j];
-                if (ch =='[' || ch == '{' || ch == '(') {
+                if (ch == '[' || ch == '{' || ch == '(') {
                     stack.push(ch);
                     continue;
                 }
+                
                 if (stack.isEmpty()) {
-                    isOk = false;
-                    break;    
-                }
-         
-                char target = stack.pop();
-                if (ch == ']' && target != '['
-                   || ch == '}' && target != '{'
-                   || ch == ')' && target != '(') {
-                    isOk = false;
+                    isValid = false;
                     break;
-                } 
+                }
+                
+                char target = stack.pop();
+                if (ch == ']' && target == '[' ||
+                   ch == '}' && target == '{' ||
+                   ch == ')' && target == '(') {
+                    continue;
+                } else {
+                    isValid = false;
+                    break;
+                }
             }
-            
-            if (isOk && stack.isEmpty()) {
+            if (isValid && stack.isEmpty()) {
                 answer++;
             }
+            
         }
         return answer;
     }
